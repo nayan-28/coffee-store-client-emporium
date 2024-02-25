25796423f16c7904282bab8a384d5467795c5bf2
nayan-28 <nayanmalakar720@gmail.com>
2024-02-25 22:26:45 +0600

    input field added

--- src/AddCoffee.jsx
+++ src/AddCoffee.jsx
@@ -2,6 +2,34 @@ const AddCoffee = () => {
   return (
     <div>
       <h2>Add Coffee</h2>
+      <form>
+        <div className="flex">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="text-group">
+              <input
+                type="text"
+                placeholder="Coffee Name"
+                className="input input-bordered"
+              ></input>
+            </label>
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="text-group">
+              <input
+                type="number"
+                placeholder="Available Quantity"
+                className="input input-bordered"
+              ></input>
+            </label>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
