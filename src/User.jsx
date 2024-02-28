ff88b4996c16ceb8bed830e193254f78924e60fa
nayan-28 <nayanmalakar720@gmail.com>
2024-02-28 23:43:05 +0600

    delete operation completed

--- src/User.jsx
+++ src/User.jsx
@@ -1,12 +1,42 @@
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const User = () => {
   const loadedUsers = useLoaderData();
   const [users, setUsers] = useState(loadedUsers);
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to recover this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/user/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deleteCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your Coffee has been deleted.",
+                icon: "success",
+              });
+            }
+            const remaining = users.filter((user) => user._id !== id);
+            setUsers(remaining);
+          });
+      }
+    });
+  };
   return (
     <div>
-      <h1 className="text-3xl text-center py-6">Users:{loadedUsers.length}</h1>
+      <h1 className="text-3xl text-center py-6">Users:{users.length}</h1>
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
@@ -15,18 +45,25 @@ const User = () => {
               <th></th>
               <th>Email</th>
               <th>Created At</th>
+              <th>Last Logged in</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
-            {loadedUsers.map((user) => (
+            {users.map((user) => (
               <tr key={user._id}>
                 <th>1</th>
                 <td>{user.email}</td>
                 <td>{user.createdAt}</td>
+                <td></td>
                 <td>
-                  <button className="btn btn-info">Action</button>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-primary"
+                  >
+                    X
+                  </button>
                 </td>
               </tr>
             ))}
